b84a2ee528b5a2726d4e6c3686de1f24b6fbb911
Caleb Kavanaugh <calebck@hotmail.com>
2019-10-04 11:45:06 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,14 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    # binding.pry
+    
+    @count_of_words = @analyzed_text.count_of_words
+    @count_of_vowels = @analyzed_text.count_of_vowels
+    @count_of_consonants = @analyzed_text.count_of_consonants
+    @most_used_letter = @analyzed_text.most_used_letter.keys[0].upcase
+    @count_of_most_used_letter = @analyzed_text.most_used_letter.values[0]
     erb :results
   end
 end
